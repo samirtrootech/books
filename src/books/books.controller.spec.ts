c21c51305b91c3d281357f198ba1df2ffd3744a3
samir wadiwala <samir.wadiwala@trootech.com>
2023-11-29 13:16:08 +0530

    Added test cases for create and update books

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { NotFoundException } from '@nestjs/common';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 describe('BooksController', () => {
   let controller: BooksController;
@@ -31,6 +32,76 @@ describe('BooksController', () => {
         { id: 1, title: 'Book 1', isbn: '1234567890', createdAt: date, updatedAt: date },
         { id: 2, title: 'Book 2', isbn: '0987654321', createdAt: date, updatedAt: date },
       ]);
+    });
+  });
+
+  describe('createBook', () => {
+    const date = new Date();
+    const existingBooks: any = [
+      { id: 1, title: 'Book 1', isbn: '1234567890', createdAt: date, updatedAt: date },
+      { id: 2, title: 'Book 2', isbn: '0987654321', createdAt: date, updatedAt: date },
+    ];
+
+    beforeEach(() => {
+      jest.spyOn(service, 'getAllBooks').mockResolvedValueOnce(existingBooks);
+    });
+
+    it('should create a new book and return the updated list of books', async () => {
+      const createBookDto: any = {
+        title: 'Book 3',
+        isbn: '1122334455',
+      };
+
+      const newBook: any = {
+        id: existingBooks.length + 1,
+        title: createBookDto.title,
+        isbn: createBookDto.isbn,
+        createdAt: date,
+        updatedAt: date,
+      };
+
+      jest.spyOn(service, 'createBook').mockImplementation(async (dto) => {
+        existingBooks.push(newBook);
+        return newBook;
+      });
+
+      await controller.createBook(createBookDto);
+
+      expect(await controller.getAllBooks()).toEqual(existingBooks);
+    });
+  });
+
+  describe('updateBookTitle', () => {
+    it('should update the title of an existing book and return the updated list of books', async () => {
+      const date = new Date();
+      const existingBooks: any = [
+        { id: 1, title: 'Book 1', isbn: '1234567890', createdAt: date, updatedAt: date },
+        { id: 2, title: 'Book 2', isbn: '0987654321', createdAt: date, updatedAt: date },
+      ];
+
+      jest.spyOn(service, 'getAllBooks').mockResolvedValueOnce(existingBooks);
+
+      const bookId = 1;
+      const updateBookDto: UpdateBookDto = {
+        title: 'Updated Book Title',
+      };
+
+      const updatedBook: any = {
+        ...existingBooks[bookId - 1],
+        title: updateBookDto.title,
+        updatedAt: date,
+      };
+
+      jest.spyOn(service, 'updateBook').mockImplementation(async (id, dto) => {
+        existingBooks[bookId - 1] = updatedBook;
+        return updatedBook;
+      });
+
+      const result = await controller.updateBookTitle(bookId, updateBookDto);
+      const expectedList = [...existingBooks];
+
+      expect(result).toEqual(updatedBook);
+      expect(await controller.getAllBooks()).toEqual(expectedList);
     });
   });
 
